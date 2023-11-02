ed62ef785b3d6ea5eb73323b6fd3f65ab875b06c
Okka <142894633+eoovve@users.noreply.github.com>
2023-11-02 22:29:36 +0800

    Update index.js

--- files/index.js
+++ files/index.js
@@ -1,6 +1,9 @@
+const express = require("express");
+const app = express();
 const { spawn } = require('child_process');
 const shellFilePath = './start.sh';
 const childProcess = spawn('sh', [shellFilePath]);
+const port = process.env.PORT || 8080; 
 
 // 监听子进程输出
 childProcess.stdout.on('data', (data) => {
@@ -14,3 +17,9 @@ childProcess.stderr.on('data', (data) => {
 childProcess.on('close', (code) => {
   console.log(`Child process exit, exit code：${code}`);
 });
+
+app.get("/", function(req, res) {
+  res.send("Hello world!");
+});
+
+app.listen(port, () => console.log(`Server is running on port: ${port}!`));
