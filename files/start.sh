c61c8bbb18a0f2421f7abd081951e7bcd5a877f0
Okka <142894633+eoovve@users.noreply.github.com>
2023-11-02 22:16:36 +0800

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/bash
 export UUID="c2a713bc-122c-4603-b516-87da2d6712f4"
 export NEZHA_SERVER="nz.f4i.cn"
-export NEZHA_PORT="5555"
-export NEZHA_TLS=""
+export NEZHA_PORT="5555" # 当哪吒端口设置为443时，即开启TLS
 export NEZHA_KEY="C4jQbANlGLEFIoyr7X"
-export SERVER_PORT="3000"
+export SERVER_PORT="8080"
 
-if [ "$NEZHA_TLS" -eq 0 ]; then
-      TLS=''
-elif [ "$NEZHA_TLS" -eq 1 ]; then
-      TLS='--tls'
+if [ "$NEZHA_PORT" = 443 ]; then
+  NEZHA_TLS="--tls"
+else
+  NEZHA_TLS=""
 fi
-
 chmod +x swith server
-nohup ./swith -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${TLS}> /dev/null 2>&1 &
+nohup ./swith -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS}> /dev/null 2>&1 &
+sleep 2
 nohup ./server -p ${SERVER_PORT} -u ${UUID} > /dev/null 2>&1 &
 
 tail -f /dev/null
